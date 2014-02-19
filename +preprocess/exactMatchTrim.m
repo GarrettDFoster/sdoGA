200f0676c1bfde3dccb71e9335bcb1b566320e2c
Garrett Foster <garrett.d.foster@gmail.com>
2014-02-19 13:29:48 -0500

    Fixed preprocess function for Matlab 2013

--- +preprocess/exactMatchTrim.m
+++ +preprocess/exactMatchTrim.m
@@ -1,5 +1,7 @@
 function candidates = exactMatchTrim(state,options)
   candidates = unique(state.candidate_tbl,'rows');
-  i = ~ismember(candidates,state.variable_tbl,'rows');
-  candidates = candidates(i,:);
+  if ~isempty(state.variable_tbl)
+    i = ~ismember(candidates,state.variable_tbl,'rows');
+    candidates = candidates(i,:);
+  end
 end
